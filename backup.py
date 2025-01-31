9f3c2d68aea0d4e823780aabdd8d53ab6c31b162
jatinparmar24 <jatinparmar2424@gmail.com>
2025-01-31 14:32:02 +0530

    else if

--- backup.py
+++ backup.py
@@ -1444,3 +1444,72 @@ else:
 
 # nested if
 
+a=int(input("Enter 1st Number = "))
+b=int(input("Enter 2nd Number = "))
+c=int(input("Enter 3rd Number = "))
+
+if (a>b and a>c):
+    print(f'{a} is greater')
+
+else:
+    if (b>c and b>a):
+        print(f'{b} is greater')
+
+    else:
+        print(f'{c} is greater')
+
+# exception = first and second num is zero and third is -1 then gives -1 bigger
+# avoid using nested if
+
+
+# if elif
+
+a=int(input("Enter 1st Number = "))
+b=int(input("Enter 2nd Number = "))
+c=int(input("Enter 3rd Number = "))
+
+if (a>b and a>c):
+    print(f'{a} is greater')
+
+elif (b>c and b>a):
+     print(f'{b} is greater')
+
+elif (c>a and c>b):
+     print(f'{c} is greater')
+
+# not work for two same number
+
+
+
+# complete for same number also
+a=int(input("Enter 1st Number = "))
+b=int(input("Enter 2nd Number = "))
+c=int(input("Enter 3rd Number = "))
+
+
+if (a==b and b==c ):
+    print("a and  b and c are equal")
+
+
+elif (a==b ):
+    print("a and  b are equal")
+
+
+elif (b==c ):
+    print("b and  c are equal")
+
+elif (c==a ):
+    print("c and  a are equal")
+
+
+elif (a>b and a>c):
+    print(f'{a} is greater')
+
+elif (b>c and b>a):
+     print(f'{b} is greater')
+
+elif (c>a and c>b):
+     print(f'{c} is greater')
+
+else:
+    print("Please Enter Valid Input")
--- first.py
+++ first.py
@@ -1,8 +1,32 @@
-x=int(input("Enter your age"))
+a=int(input("Enter 1st Number = "))
+b=int(input("Enter 2nd Number = "))
+c=int(input("Enter 3rd Number = "))
 
 
-if x>=18:
-    print("u can vote")
+if (a==b and b==c ):
+    print("a and  b and c are equal")
 
-else:
-    print("you can't vote")
+elif (a>b and a>c):
+    print(f'{a} is greater')
+
+elif (b>c and b>a):
+     print(f'{b} is greater')
+
+elif (c>a and c>b):
+     print(f'{c} is greater')
+
+elif (a==b ):
+    print("a and  b are equal")
+
+
+elif (b==c ):
+    print("b and  c are equal")
+
+elif (c==a ):
+    print("c and  a are equal")
+
+
+
+
+
+
