4a1e3d2e87c48292932e3056f0f46f6e6e77edb4
jatinparmar24 <jatinparmar2424@gmail.com>
2025-06-26 21:36:33 +0530

    new question swap first and last

--- backup.py
+++ backup.py
@@ -4244,3 +4244,28 @@ st=input("Enter a Word =")
 def count_vowel(st):
     return sum(1 for ch in st.lower() if ch in 'aeiou')
 print(count_vowel(st))
+
+# for string
+user_input = input("Enter a string: ")
+def swap_first_last_string(s):
+    if len(s) < 2:
+        return s
+    return s[-1] + s[1:-1] + s[0]
+
+result = swap_first_last_string(user_input)
+
+print("After swapping first and last characters:", result)
+
+
+# for list
+user_input = input("Enter elements of the list separated by space: ")
+def swap_first_last_list(lst):
+    if len(lst) < 2:
+        return lst
+    lst[0], lst[-1] = lst[-1], lst[0]
+    return lst
+
+lst = list(map(int, user_input.split()))
+result = swap_first_last_list(lst)
+
+print("After swapping first and last:", result)
--- first.py
+++ first.py
@@ -1,9 +1,24 @@
-for i in range(1, 16):
-    if i % 3 == 0 and i % 5 == 0:
-        print("FizzBuzz")
-    elif i % 3 == 0:
-        print("Fizz")
-    elif i % 5 == 0:
-        print("Buzz")
-    else:
-        print(i)
+# for string
+user_input = input("Enter a string: ")
+def swap_first_last_string(s):
+    if len(s) < 2:
+        return s
+    return s[-1] + s[1:-1] + s[0]
+
+result = swap_first_last_string(user_input)
+
+print("After swapping first and last characters:", result)
+
+
+# for list
+user_input = input("Enter elements of the list separated by space: ")
+def swap_first_last_list(lst):
+    if len(lst) < 2:
+        return lst
+    lst[0], lst[-1] = lst[-1], lst[0]
+    return lst
+
+lst = list(map(int, user_input.split()))
+result = swap_first_last_list(lst)
+
+print("After swapping first and last:", result)
